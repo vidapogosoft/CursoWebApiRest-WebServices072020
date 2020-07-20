1f6a6dce1b63edd507c07c1f4d3fac44e290cd06
vidapogosoft <vidapogosoft@hotmail.com>
2020-07-20 10:25:10 -0500

    Taller dia 1
    ejemplo de api rest conectado a sql server
    uso de scadffold

---
+++ DemoDevWebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+using System.Web.Http.Cors;
+
+namespace DemoDevWebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+
+            //Habilitando Cors
+            var urlPermitidas = new EnableCorsAttribute("*"
+                                                ,"*","*");
+            config.EnableCors(urlPermitidas);
+
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
