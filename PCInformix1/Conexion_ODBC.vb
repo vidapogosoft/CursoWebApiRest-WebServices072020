a07791356682c5e33b4354e7ab3472cd5cc6d715
vidapogosoft <vidapogosoft@hotmail.com>
2020-08-11 11:42:56 -0500

    Demo 1
    Prueba de concepto 1 de conexion a informix

---
+++ PCInformix1/Conexion_ODBC.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Data.Odbc
+
+Module Conexion_ODBC
+
+    Dim conexionODBC As New OdbcConnection
+
+    Public Sub Conectar(conexion As String)
+        conexionODBC.ConnectionString = conexion
+
+        Try
+
+            conexionODBC.Open()
+
+        Catch ex As OdbcException
+            MsgBox(ex.ToString())
+        End Try
+
+    End Sub
+
+    Public Sub Cerrar()
+        conexionODBC.Close()
+    End Sub
+
+
+    Public Function Consultar(query As String) As DataTable
+        Dim datos As New DataTable
+        Dim adaptador As New OdbcDataAdapter(query, conexionODBC)
+        adaptador.Fill(datos)
+        Return datos
+    End Function
+
+    Public Function ConexionInformix() As String
+        Dim cnx As String
+
+        cnx = "
+            
+        Driver ={IBM INFORMIX ODBC DRIVER};
+        Database = miBD;
+        Host = MI IP desde donde me estoy conectando;
+        Server = miServidor;
+        Services = 50000;
+        Protocol = onsoctcp;
+        UID = miuser;
+        PWD = mipwd;
+
+        "
+        Return cnx
+
+    End Function
+
+
+End Module
---
+++ PCInformix1/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Conectar(ConexionInformix)
+
+        dgTabla.DataSource = Consultar("Select * from MiTabla")
+
+    End Sub
+End Class
